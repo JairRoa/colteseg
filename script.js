6f7b974ab593e0f1ff1b5cd94351d02efb05b5b5
JairRoaCalvo <jair.roa.calvo@gmail.com>
2024-05-22 21:11:59 -0500

    corrección navbar

--- script.js
+++ script.js
@@ -6,40 +6,44 @@ function seleccionar() {
   MenuVisible = false;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Aquí va tu código JavaScript
-  let MenuVisible = false;
+//======== MENÚ ============//
 
-  function mostrarOcultarMenu() {
-      let nav = document.getElementById("nav");
-      if (MenuVisible) {
-          nav.classList.remove("responsive");
-          document.getElementById("menu_hamburguesa").checked = false; // Desactiva la casilla de verificación
-          MenuVisible = false;
-      } else {
-          nav.classList.add("responsive");
-          MenuVisible = true;
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+  const checkbox = document.getElementById('menu_hamburguesa');
+
+  for (const link of links) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - document.querySelector('.navbar').offsetHeight,
+          behavior: "smooth",
+        });
+
+        // Desmarca el checkbox para contraer el menú
+        checkbox.checked = false;
       }
+    });
   }
 });
 
 
 
-
-
-
-
-
 // ========== SLIDER =========== //
-document.addEventListener("DOMContentLoaded", function() {
-  const slider = document.querySelector('.slider');
-  const slides = document.querySelectorAll('.slide');
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
   const slideCount = slides.length;
   let currentIndex = 0;
   let intervalId; // Variable para almacenar el ID del intervalo
 
   function showSlide(index) {
-    const offset = -index * 100 + '%'; // Calcula el desplazamiento necesario para mostrar la imagen actual
+    const offset = -index * 100 + "%"; // Calcula el desplazamiento necesario para mostrar la imagen actual
     slider.style.transform = `translateX(${offset})`; // Aplica el desplazamiento al slider
   }
 
@@ -64,16 +68,16 @@ document.addEventListener("DOMContentLoaded", function() {
   startSlider(); // Inicia el slider automáticamente
 
   // Agrega eventos de clic a botones de avance y retroceso
-  const prevButton = document.querySelector('.prev-button');
-  const nextButton = document.querySelector('.next-button');
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
 
-  prevButton.addEventListener('click', function() {
+  prevButton.addEventListener("click", function () {
     stopSlider(); // Detiene el slider cuando se hace clic en el botón de retroceso
     prevSlide();
     startSlider(); // Reinicia el slider después de retroceder
   });
 
-  nextButton.addEventListener('click', function() {
+  nextButton.addEventListener("click", function () {
     stopSlider(); // Detiene el slider cuando se hace clic en el botón de avance
     nextSlide();
     startSlider(); // Reinicia el slider después de avanzar
@@ -84,37 +88,48 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // ======= COTIZADOR ========//
 
-document.addEventListener('DOMContentLoaded', function () {
-  const calcularBtn = document.getElementById('calcular');
-  const productoSelect = document.getElementById('producto');
-  const cantidadSelect = document.getElementById('cantidad');
-  const valorTotalSpan = document.getElementById('valor-total');
+document.addEventListener("DOMContentLoaded", function () {
+  const calcularBtn = document.getElementById("calcular");
+  const productoSelect = document.getElementById("producto");
+  const cantidadSelect = document.getElementById("cantidad");
+  const valorTotalSpan = document.getElementById("valor-total");
 
-  calcularBtn.addEventListener('click', function () {
-      const precio = parseInt(productoSelect.options[productoSelect.selectedIndex].getAttribute('data-precio'));
-      const cantidad = parseInt(cantidadSelect.value);
-      const valorTotal = precio * cantidad;
-      valorTotalSpan.textContent = valorTotal ? `\$${valorTotal.toLocaleString()}` : '';
+  calcularBtn.addEventListener("click", function () {
+    const precio = parseInt(
+      productoSelect.options[productoSelect.selectedIndex].getAttribute(
+        "data-precio"
+      )
+    );
+    const cantidad = parseInt(cantidadSelect.value);
+    const valorTotal = precio * cantidad;
+    valorTotalSpan.textContent = valorTotal
+      ? `\$${valorTotal.toLocaleString()}`
+      : "";
   });
 });
 
+
+
 // ======== RESERVAR ========//
 function enviarWhatsApp(element) {
-  const productElement = element.closest('.product');
-  const h2Content = productElement.querySelector('h2').textContent;
+  const productElement = element.closest(".product");
+  const h2Content = productElement.querySelector("h2").textContent;
   const message = `Quiero reservar ${encodeURIComponent(h2Content)}`;
-  const phoneNumber = '573224142500'; // Número de teléfono de Colombia
+  const phoneNumber = "573224142500"; // Número de teléfono de Colombia
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
   window.location.href = whatsappURL;
 }
 
 
+
 // ======== MAS INFO ========//
 function enviarWhatsApp(element) {
-  const productElement = element.closest('.instalacion-item');
-  const h3Content = productElement.querySelector('h3').textContent;
-  const message = `Requiero más información acerca de ${encodeURIComponent(h3Content)}`;
-  const phoneNumber = '573224142500'; // Número de teléfono de Colombia
+  const productElement = element.closest(".instalacion-item");
+  const h3Content = productElement.querySelector("h3").textContent;
+  const message = `Requiero más información acerca de ${encodeURIComponent(
+    h3Content
+  )}`;
+  const phoneNumber = "573224142500"; // Número de teléfono de Colombia
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
   window.location.href = whatsappURL;
 }
